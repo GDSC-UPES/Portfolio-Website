21e50c27aa9f0c0f003b6ac118ac086101128794
“xBlackStormx;[]] <[amanpachisia14@gmail.com]>
2023-02-08 01:13:40 +0530

    Updated JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,8 @@
 function add()
 {
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
+
+    num1 = parseInt(document.getElementById("firstNumber").value);
+    num2 = parseInt(document.getElementById("secondNumber").value);
     document.getElementById("result").innerHTML = num1 + num2;
 }
 
